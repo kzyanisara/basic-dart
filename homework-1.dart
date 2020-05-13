95e79ffd35aa696d0e7e3eab3bd18cb45f255ae7
kzyanisara <kzyanisara@yahoo.com>
2020-05-13 20:25:27 +0700

    fix age

--- homework-1.dart
+++ homework-1.dart
@@ -1,14 +1,11 @@
-main(){
-int age = 49;
+main() {
+  int age = 48;
 
-if (age < 20) {
-  print('Young');
-}
-else if ( 50 > age && age > 20) {
-  print('Medium');
-}
-else {
-  print('Old');
-}
-
+  if (age < 20) {
+    print('Young');
+  } else if (50 > age && age > 20) {
+    print('Medium');
+  } else {
+    print('Old');
+  }
 }
